c9668a09614ba4843903961e1ea0aea8bce7d9a7
Rendra.aja <itzslaiming53@gmail.com>
2024-08-17 07:04:19 -0700

    Add files via upload

---
+++ handler/variables.js
@@ -0,0 +1,11 @@
+module.exports = (client) => {
+    client.variables({
+        prefix: "",
+        embedcolor: "#00FFFF",
+        aichannel:"",
+        aimsg:"",
+        autopingreply: "on",
+        readyChannel: "1272920183245373451", // Change this to your ready channel id
+        aiChannel: "" // don't change this or fill it with your channel id
+    })
+}
